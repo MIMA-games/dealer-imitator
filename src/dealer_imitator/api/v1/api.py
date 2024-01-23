d4f61e1d2bf626110461ac82bacc76d3e6d95fa5
Miriani Chkhvimiani <mirianchkhvimiani@gmail.com>
2024-01-23 15:19:23 +0400

    Added API endpoints for launching the dealer imitator for different games: blackjack, baccarat, casino_poker

--- src/dealer_imitator/api/v1/api.py
+++ src/dealer_imitator/api/v1/api.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
-from .endpoints.blackjack import router as game_router
+from .endpoints.blackjack import router as blackjack_router
+from .endpoints.baccarat import router as baccarat_router
+from .endpoints.casino_poker import router as casino_poker_router 
 
 
 router = APIRouter()
-router.include_router(game_router, prefix="/blackjack", tags=["blackjack"])
+router.include_router(blackjack_router, prefix="/blackjack", tags=["blackjack"])
+router.include_router(baccarat_router, prefix="/baccarat", tags=["baccarat"])
+router.include_router(casino_poker_router, prefix="/casino_poker", tags=["casino_poker"])
 
 
 @router.get("/health", tags=["general"])
---
+++ src/dealer_imitator/api/v1/endpoints/casino_poker.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from fastapi import APIRouter, Depends
+
+from dealer_imitator.connections.async_redis_conn import r
+from dealer_imitator.connections.mongodb_conn import db
+from dealer_imitator.connections.sio_client import sio_client
+from dealer_imitator.common.enums import Games
+from dealer_imitator.common.cards.shoe import Shoe
+
+
+router = APIRouter()
+
+
+@router.get("/connect/{game_id}", status_code=200)
+async def connect_dealer(game_id: str):
+    shoe = Shoe.setup(6)
+    await r.remove_all_remaining_cards(game_id)
+    await r.add_initial_cards_list(game_id, list(shoe.shoe.keys()))
+
+    asyncio.create_task(sio_client.start_socketio(Games.CASINO_POKER, game_id))
+
+    return {"message": f"Connecting to Socket.IO for game_id={game_id}"}
